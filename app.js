3432e6e91a1fa6f9901509a977b5bbbd1b445dbd
Jay Tailor <tailorj64@gmail.com>
2019-04-24 16:06:20 +0530

    Implemented Image upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const multer = require('multer')
+const ejs = require('ejs')
+const path = require('path')
+
+const PORT = 3030;
+// Setup Storage engine
+const storage = multer.diskStorage({
+    destination: './public/uploads',
+    filename: (req,file,cb) => {
+        cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+}); 
+
+// Initialize Image Upload
+const upload = multer({
+    storage: storage,
+}).single('myImage');
+
+// Initialize 
+const app = express();
+
+// EJS
+app.set('view engine','ejs')
+
+// Public folder
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => res.render('index'))
+
+app.post('/upload', (req,res) => {
+    upload(req,res, (err) => {
+        if(err) {
+            res.render('index',{
+                msg: err
+            })
+        } else {
+            console.log(req.file);
+            res.send('test')
+        }
+    })
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
